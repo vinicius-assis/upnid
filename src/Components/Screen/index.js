baff6722cd017fc5a159885eb9d92ebb3061ef1b
Vinicius Assis <believe.182@hotmail.com>
2019-11-29 16:28:28 -0300

    Create a function to start the game

--- src/Components/Screen/index.js
+++ src/Components/Screen/index.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
-//import Background from '../../assets/fundo-carro.gif'
+import Background from '../../assets/fundo-carro.gif'
 import BackgroundPaused from '../../assets/paused-fundo.png'
-import InfoBar from '../InforBar'
+import InfoBar from '../InfoBar'
 import CarWrapper from '../Car'
 import PausedLayer from '../PausedLayer'
 import PlayerForm from '../PlayerForm'
@@ -11,15 +11,20 @@ import PlayerForm from '../PlayerForm'
 const Screen = styled.div`
   width: 100vw;
   height: 100vh;
-  background-image: url(${BackgroundPaused});
+  background-image: url(${props => props.start === 'true' ? Background : BackgroundPaused });
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
 `
 
 const ScreenWrapper = () => {
+  const [startGame, setStartGame] = useState('false')
   const [position, setPosition] = useState('middle')
   const [nickname, setNickname] = useState('Unknown')
+
+  function playGame() {
+    setStartGame('true')
+  }
 
   function moveCar(event) {
     switch(event.key) {
@@ -34,18 +39,22 @@ const ScreenWrapper = () => {
     }   
   }
 
+
+  //Function for get a username from form and add in InfoBar
   function formSubmit(event) {
     event.preventDefault()
     let username = event.target.username.value
     if (username !== '') {
       setNickname(username)
-    } 
+    }
+    playGame()
+    console.log(startGame)
   }
 
   useEffect(() => document.addEventListener('keypress', moveCar))
 
   return (
-    <Screen >
+    <Screen start={startGame}>
       <PausedLayer>
         <PlayerForm event={formSubmit} />
       </PausedLayer>
