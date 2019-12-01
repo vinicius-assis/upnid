6a4f4e36d57f576468fa8c033f9c228acb84e59e
Vinicius Assis <believe.182@hotmail.com>
2019-12-01 16:44:56 -0300

    Updates

---
+++ precache-manifest.7bdc6fb837b479068157d6ccc4ef4600.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc212f964675b571209c23e75b7aa1d",
+    "url": "/upnid/index.html"
+  },
+  {
+    "revision": "07f5989a16271a3e22c6",
+    "url": "/upnid/static/js/2.842e9a4a.chunk.js"
+  },
+  {
+    "revision": "bf192951d7b46e988202",
+    "url": "/upnid/static/js/main.64366b3b.chunk.js"
+  },
+  {
+    "revision": "6a67fe41b6fa2668d72e",
+    "url": "/upnid/static/js/runtime-main.7edecc76.js"
+  },
+  {
+    "revision": "56be0a4e7be4b661072436aa22f66f64",
+    "url": "/upnid/static/media/carro.56be0a4e.png"
+  },
+  {
+    "revision": "d0d2cd1752d215c4df68e76e976ec629",
+    "url": "/upnid/static/media/fundo-carro.d0d2cd17.gif"
+  },
+  {
+    "revision": "57d18c589519b81464d15710562c1f40",
+    "url": "/upnid/static/media/paused-fundo.57d18c58.png"
+  }
+]);
